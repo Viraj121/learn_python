c62363bea418330913855e11208cc51525a83797
Viraj121 <gavasviraj@gmail.com>
2024-12-16 16:36:02 +0530

    updates

--- oops.py
+++ oops.py
@@ -1,8 +1,21 @@
 class Student:
-    name="karan"
+    
+    college_name="abc college"
 
-s1=Student()
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+
+    def welcome(self):
+        print("welcome student,",self.name)
+
+    def get_marks(self):
+        return self.marks
+    
+
+
+s1=Student("karan",90)
+
 print(s1.name)
-
-s2=Student()
-print(s2.name)
+s1.welcome()
+print(s1.get_marks())
